47d5c502ffa859e97dbe4e42818fadb7167f75a6
Sabeel28x <138629101+Sabeel28x@users.noreply.github.com>
2024-08-28 22:50:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Server IP Address</title>
+<title>Server IP</title>
 </head>
 <body>
 <h1>My Server IP address is: <?php echo $_SERVER['SERVER_ADDR']; ?></h1>
